7942cf9fcf7127e3b8004084ef68e5c42799089f
bombi <bombic3@naver.com>
2022-07-12 10:00:36 +0900

    REFACTOR : async/await 적용하기

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ import axios from "../node_modules/axios/index";
 
 function App () {
   const [data, setData] = useState(null);
-  const onClick = () => {
-    axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/todos/1',
+      );
       setData(response.data);
-    });
+    } catch (e) {
+      console.log(e);
+    }
   };
+
+  // const onClick = () => {
+  //   axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
+  //     setData(response.data);
+  //   });
+  // };
   
   return (
     <div>
