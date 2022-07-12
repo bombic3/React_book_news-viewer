f4ce6e5fede8f274d004f5f2db9110b3cec97ebd
bombi <bombic3@naver.com>
2022-07-12 10:17:51 +0900

    FEAT : newsapi 불러오기

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App () {
   const onClick = async () => {
     try {
       const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos/1',
+        // 기존에 사용했던 가짜 API인 JSONPlaceholder API 를 전체 뉴스 불러오는 API로 대체하기
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=432e7b719e4645c29a1fc258b7adf8d4',
       );
       setData(response.data);
     } catch (e) {
