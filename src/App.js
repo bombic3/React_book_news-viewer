1609a93a7b0cb8747d0071fdd62e517f0d07d792
bombi <bombic3@naver.com>
2022-09-02 17:45:32 +0900

    FIX : title 수정

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import { Helmet } from 'react-helmet-async';
 import { Route, Routes } from 'react-router-dom';
 import NewsPage from './pages/NewsPage';
 
 const App = () => {
   return (
-    <>
-      <Helmet>
-        <title>NEWS VEWER</title>
-      </Helmet>
-      <Routes>
-        <Route path="/" element={<NewsPage />} />
-        <Route path="/:category" element={<NewsPage />} />
-      </Routes>
-      </>
+    <Routes>
+      <Route path="/" element={<NewsPage />} />
+      <Route path="/:category" element={<NewsPage />} />
+    </Routes>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import { HelmetProvider } from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+  <BrowserRouter>    
+    <App />
   </BrowserRouter>
 );
